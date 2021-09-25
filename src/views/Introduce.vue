86b95250cca0e10a86ba3e2a706c95ac717b7712
choisungwook <kgg1959@naver.com>
2021-09-25 18:03:13 +0900

    소개 페이지 추가

--- src/views/Introduce.vue
+++ src/views/Introduce.vue
@@ -1,6 +1,19 @@
 <template>
-  <v-container>
-    <div>This is INtroduce</div>
+  <v-container class="pt-10">
+    <v-row>
+      <p class="white--text headline">
+        안녕하세요. 2020년 11월부터 Devops엔지니어로 일하고 있습니다. <br />
+        쿠버네티스, 클라우드, 백엔드 분야 중심으로 공부하고 있습니다. <br />
+      </p>
+    </v-row>
+    <v-row class="pt-15">
+      <p class="white--text headline">
+        공부한 것을 github, 블로그, 유투브로 정리하고 있습니다. <br />
+        github 링크: https://github.com/choisungwook <br />
+        블로그 링크: https://malwareanalysis.tistory.com/<br />
+        유투브 링크: https://www.youtube.com/c/악분일상 <br />
+      </p>
+    </v-row>
   </v-container>
 </template>
 
